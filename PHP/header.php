15739b4c4b273e15ff06de96170a23d3e5841299
dcjc29 <durekshawasala@gmail.com>
2018-08-29 23:30:52 +0530

    homepage.php added

---
+++ PHP/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home Page</title>
+<link rel="stylesheet" type="text/css" href="CSS\Stylesheet.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<div style="position: fixed; width: 100%;">	    
+<header>
+<h1>HIRED</h1>
+<div style="float: right;">
+<img href="">    
+<a class="account" href="Login.html">Log In</a>&nbsp&nbsp;
+<a class="account" href="Registration.html">Register</a>
+</div>
+</header>
+<hr>
+<div>
+<nav>
+    <ul>
+        <li><a href="homepage.html" >Home</a></li>
+        <li><a href="jobs.html" >Jobs</a></li>
+        <li><a href="companies.html" >Companies</a></li>
+        <li><a href="AboutUs.html">About Us</a></li>
+        <li><a href="ContactUs.html">Contact Us</a></li>
+    </ul>    
+</nav>
+</div>
---
+++ PHP/homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home Page</title>
+<link rel="stylesheet" type="text/css" href="CSS\Stylesheet.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php 
+    include "header.php";
+?>
+<hr>
+</div>
+<div class="contentHome"></div>
+<h3 id="home">Who Are You?</h3>
+
+<div class="row"></div>
+    <a href="Login.html"><div class="hcolumn"><img src="src\img_507320.png"><br><br><br>A Job Seeker</div></a>
+    <a href="Login.html"><div class="hcolumn"><img src="src\img_457957.png"><br><br>A Company</div></a>
+    <a href="Login.html"><div class="hcolumn"><img src="src\img_506378.png"><br>Administrator</div></a>
+</div>    
+</div>  
+<?php 
+    include "footer.php";
+?>
+</body>
+
+</html>
